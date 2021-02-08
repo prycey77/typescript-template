4308d1889863487e5b1049a104f971c0cdae3368
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-02-08 18:13:07 +0000

    Add ts-jest and an example test for helloWorld()

--- src/hello.ts
+++ src/hello.ts
@@ -1,7 +1,9 @@
 /**
  * A hello world function.
+ * @return {string} the string that was logged to the console
  */
 export function helloWorld() {
   const message: string = 'Hello, World!';
   console.log(message);
+  return message;
 }
