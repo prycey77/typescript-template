1fc3cf2c5205e1eb1b5c61cbb135e30ab0b46cbe
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-02-15 17:05:09 +0000

    Add prettier command and format all .json and .js with it

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    // https://prettier.io/docs/en/options.html
-    semi: true,
-    trailingComma: 'es5',
-    singleQuote: false,
-    printWidth: 100,
-    tabWidth: 2,
-    useTabs: false,
-}
+  // https://prettier.io/docs/en/options.html
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+};
