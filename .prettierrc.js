fd8ddcf5ba8f95970173e7de9b0b054836ac584d
Faizaan <faizaan.sakib@bardsley-england.com>
2021-06-07 10:52:15 +0100

    feat: use eslint-config-lint

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // https://prettier.io/docs/en/options.html
-  semi: true,
-  trailingComma: "es5",
-  singleQuote: false,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-};
